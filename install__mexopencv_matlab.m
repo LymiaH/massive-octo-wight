b8f63b3e05582e71b2347737267608f8a710ae0d
LymiaH <LymiaH@users.noreply.github.com>
2017-05-16 11:03:12 +0800

    Updated install for mexopencv to work for MATLAB

--- install__mexopencv_matlab.m
+++ install__mexopencv_matlab.m
@@ -1,12 +1,11 @@
 path_root = pwd;
-%change this to where you set up the libaries
+%change this to where you have the libaries
 path_dev = fullfile(fileparts(path_root),'dev');
 cd(fullfile(path_dev,'mexopencv'));
-path_mexopencv = fullfile(path_dev,'mexopencv')
+path_mexopencv = fullfile(path_dev,'mexopencv');
 addpath(path_mexopencv);
 addpath(fullfile(path_mexopencv,'opencv_contrib'));
 path_opencv = fullfile(path_dev,'build','install');
 addpath(path_opencv);
-%Uncomment this if you need to compile
-%mexopencv.make('opencv_path',path_opencv,'opencv_contrib',true);
+mexopencv.make('opencv_path',path_opencv,'opencv_contrib',true);
 cd(path_root);
