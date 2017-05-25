5f05014f2f5b359ac269e8e0c94a08bb66354bf0
LymiaH <LymiaH@users.noreply.github.com>
2017-05-25 11:38:50 +0800

    Added sample scripts

---
+++ script_to_read_PennFudan_data.m
@@ -0,0 +1,21 @@
+addpath('./PAScode')
+%% 
+% From project description page
+baseDir = '.\';
+annotDir = [baseDir 'PennFudanPed\Annotation\'];
+
+files = dir(annotDir); files(1:2) = [];
+close all;
+for ii = 1 : length(files)
+    fileName = [annotDir files(ii).name];
+    record = PASreadrecord(fileName);
+    input_image = imread([baseDir record.imgname]);
+    main_img = imshow(input_image); hold on;
+    for jj = 1 : length(record.objects)
+        bbox = record.objects(jj).bbox;
+        bbox(3:4) = bbox(3:4) - bbox(1:2);
+        rectangle('Position', bbox, 'EdgeColor','g','LineWidth',2);
+    end
+    hold off;
+    pause(1);
+end
---
+++ script_to_read_PennFudan_data_modified.m
@@ -0,0 +1,25 @@
+addpath(fullfile(pwd,'PAScode'))
+%%
+% From project description page
+annotDir = fullfile(pwd,'PennFudanPed','Annotation');
+
+files = dir(annotDir); files(1:2) = [];
+close all;
+for ii = 1 : length(files)
+    fileName = [annotDir files(ii).name];
+    record = PASreadrecord(fileName);
+    input_image = imread(fullfile(pwd, record.imgname));
+    main_img = imshow(input_image); hold on;
+    for jj = 1 : length(record.objects)
+        pause(0.05);
+        bbox = record.objects(jj).bbox;
+        bbox(3:4) = bbox(3:4) - bbox(1:2);
+        img_over = rgb2gray(imcrop(input_image, bbox));
+        img_over_h = imshow(img_over);
+        set(img_over_h, 'XData', get(img_over_h, 'XData') + bbox(1) - 1);
+        set(img_over_h, 'YData', get(img_over_h, 'YData') + bbox(2) - 1);
+        rectangle('Position', bbox, 'EdgeColor','g','LineWidth',2);
+    end
+    hold off;
+    pause(0.2);
+end
