ae04cef419d0c6de3ed5ca6845969751cc3dc070
LymiaH <LymiaH@users.noreply.github.com>
2017-05-30 09:14:13 +0800

    Added quadrants of positives to negatives

--- generate_negatives.m
+++ generate_negatives.m
@@ -1,13 +1,6 @@
 function [ negatives ] = generate_negatives( record )
 %GENERATE_NEGATIVES Generates negative sample bounding boxes
-    %Create a collision mask from the bounding boxes
-%     collision_mask = zeros(record.imgsize(1), record.imgsize(2))
-%     for jj = 1 : length(record.objects)
-%         bbox = record.objects(jj).bbox;
-%         bbox(3:4) = bbox(3:4) - bbox(1:2);
-%         mask_fill_rectangle(collision_mask, bbox(1), bbox(2), bbox(3),
-%         bbox(4))
-%     end
+    
     %calculate the collision boxes
     bboxes = cell(1, length(record.objects));
     for jj = 1 : length(record.objects)
@@ -20,6 +13,7 @@ function [ negatives ] = generate_negatives( record )
     for jj = 1 : length(record.objects)
         bbox = record.objects(jj).bbox;
         bbox(3:4) = bbox(3:4) - bbox(1:2);
+        %Randomly shift the box such that it does not overlap
         found = false;
         for atmp = 1:100
             bbox(1) = randi([1,record.imgsize(1) - bbox(3)]);
@@ -38,7 +32,14 @@ function [ negatives ] = generate_negatives( record )
             break
         end
         if found
-           negatives{length(negatives) + 1} = bbox;
+           negatives{end + 1} = bbox;
         end
+        %Divide the positive into quandrants for negatives
+        bbox = record.objects(jj).bbox;
+        bbox_hsize = floor(bbox(3:4) - bbox(1:2))./2;
+        negatives{end + 1} = [bbox(1), bbox(2), bbox_hsize(1), bbox_hsize(2)];
+        negatives{end + 1} = [bbox(1) + bbox_hsize(1), bbox(2), bbox_hsize(1), bbox_hsize(2)];
+        negatives{end + 1} = [bbox(1), bbox(2) + bbox_hsize(2), bbox_hsize(1), bbox_hsize(2)];
+        negatives{end + 1} = [bbox(1) + bbox_hsize(1), bbox(2) + bbox_hsize(2), bbox_hsize(1), bbox_hsize(2)];
     end
 end
